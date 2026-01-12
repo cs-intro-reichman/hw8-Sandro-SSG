36b22e9261e6f8927032e89ca871354a6345c5bc
unknown <Sandro-SSG>
2026-01-12 20:34:40 +0200

    Complited

--- User.java
+++ User.java
@@ -107,9 +107,13 @@
 
     /** Checks is this user is a friend of the other user.
      *  (if two users follow each other, they are said to be "friends.") */
-    public boolean isFriendOf(User other) {
-        if(other.follows(name)) return true;
-        return false;
+   public boolean isFriendOf(User other) {
+
+        boolean iFollowHim = this.follows(other.getName());
+
+        boolean heFollowsMe = other.follows(this.name);
+        
+        return iFollowHim && heFollowsMe;
     }
 
     /** Returns this user's name, and the names that s/he follows. */
